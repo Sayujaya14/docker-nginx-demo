ea07291a9640a74aec51459c3b1e81498b76489c
Sayujaya Sharma <Sayujaya.Sharma@mu-sigma.com>
2026-05-21 19:13:30 +0530

    enhancement : integrated multiple containers

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const os = require("os");
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello from Backend");
+app.get("/api", (req, res) => {
+  res.send(`Hello from backend: ${os.hostname()}`);
+
 });
 
 app.listen(3000, () => {
-  console.log("Backend running on 3000");
+  console.log("Backend running on port 3000");
 });
